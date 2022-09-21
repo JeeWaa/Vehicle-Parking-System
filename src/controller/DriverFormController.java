89926e3d2788d12a0d0e182b9be89fdac6a7d300
JeeWaa <jeewa228@gmail.com>
2022-09-21 19:59:41 +0530

    driver

---
+++ src/controller/DriverFormController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.scene.input.MouseEvent;
+
+public class DriverFormController {
+    public JFXTextField driverNameText;
+    public JFXTextField nicText;
+    public JFXTextField driverLicenseText;
+    public JFXTextField addressText;
+    public JFXTextField contactText;
+    public JFXButton addDriverButton;
+
+    public void addDriver(MouseEvent mouseEvent) {
+    }
+}
