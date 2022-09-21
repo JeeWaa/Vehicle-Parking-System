eeaeec8336a98a4edc123a2120f94e0db4b3684a
JeeWaa <jeewa228@gmail.com>
2022-09-21 19:02:24 +0530

    delivery

---
+++ src/controller/DeliveryFormController.java
@@ -0,0 +1,28 @@
+package controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
+
+public class DeliveryFormController {
+    public JFXComboBox deliveryComboBox;
+    public TableView tableView;
+    public TableColumn vehicleNumberColumn;
+    public TableColumn vehicleTypeColumn;
+    public TableColumn driverNameColumn;
+    public TableColumn leftTimeColumn;
+    public JFXButton logOutButton;
+    public JFXButton addVehicleButton;
+    public JFXButton addDriverButton;
+
+    public void addVehicle(MouseEvent mouseEvent) {
+    }
+
+    public void addDriver(MouseEvent mouseEvent) {
+    }
+
+    public void logOut(MouseEvent mouseEvent) {
+    }
+}
