90cd7635e3851cad974753ebae6b5d7b0dc71f7c
JeeWaa <jeewa228@gmail.com>
2022-09-21 16:04:29 +0530

    parking

---
+++ src/controller/ParkingFormController.java
@@ -0,0 +1,28 @@
+package controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
+
+public class ParkingFormController {
+    public JFXComboBox parkingComboBox;
+    public TableView tableView;
+    public TableColumn vehicleNumberColumn;
+    public TableColumn vehicleTypeColumn;
+    public TableColumn parkingSlotColumn;
+    public TableColumn parkedTimeColumn;
+    public JFXButton logOutButton;
+    public JFXButton addVehicleButton;
+    public JFXButton addDriverButton;
+
+    public void addVehicle(MouseEvent mouseEvent) {
+    }
+
+    public void addDriver(MouseEvent mouseEvent) {
+    }
+
+    public void logOut(MouseEvent mouseEvent) {
+    }
+}
