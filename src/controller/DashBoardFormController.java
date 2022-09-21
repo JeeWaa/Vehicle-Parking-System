7fa4e54203996538c65400052f7e05c0f44871de
JeeWaa <jeewa228@gmail.com>
2022-09-21 14:28:44 +0530

    dashboard

--- src/controller/DashBoardFormController.java
+++ src/controller/DashBoardFormController.java
@@ -1,4 +1,28 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
 public class DashBoardFormController {
+    public Label time;
+    public Label date;
+    public JFXComboBox vehicleComboBox;
+    public JFXComboBox driverComboBox;
+    public Label vehicleTypeText;
+    public Label driverText;
+    public Label slotNumberText;
+    public JFXButton loginButton;
+    public JFXButton onDeliveryButton;
+    public JFXButton parkVehicleButton;
+
+    public void parkVehicle(MouseEvent mouseEvent) {
+    }
+
+    public void onDeliveryShift(MouseEvent mouseEvent) {
+    }
+
+    public void managerLogin(MouseEvent mouseEvent) {
+    }
 }
