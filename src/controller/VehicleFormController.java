9ff8e5feccd340f22b68bd5f0e42807d0d08824d
JeeWaa <jeewa228@gmail.com>
2022-09-21 19:42:33 +0530

    vehicle

---
+++ src/controller/VehicleFormController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import javafx.scene.input.MouseEvent;
+
+public class VehicleFormController {
+    public JFXTextField vehicleNumberText;
+    public JFXTextField maximumWeightText;
+    public JFXTextField passengerText;
+    public JFXComboBox vehicleComboBox;
+    public JFXButton addVehicleButton;
+
+    public void addVehicle(MouseEvent mouseEvent) {
+    }
+}
