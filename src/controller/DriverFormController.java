605e53ae9494082a2ee82e47200709aa60af0df9
JeeWaa <jeewa228@gmail.com>
2022-09-23 15:46:18 +0530

    add driver

--- src/controller/DriverFormController.java
+++ src/controller/DriverFormController.java
@@ -2,7 +2,17 @@ package controller;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
+import model.Driver;
+import views.table.DriverTable;
+
+import java.util.ArrayList;
 
 public class DriverFormController {
     public JFXTextField driverNameText;
@@ -10,8 +20,45 @@ public class DriverFormController {
     public JFXTextField driverLicenseText;
     public JFXTextField addressText;
     public JFXTextField contactText;
+    public TableView<DriverTable> driverTable;
+    public TableColumn nameColumn;
+    public TableColumn nicColumn;
+    public TableColumn licenseColumn;
+    public TableColumn addressColumn;
+    public TableColumn contactColumn;
     public JFXButton addDriverButton;
 
+    public static ArrayList<Driver> drivers = new ArrayList<>();
+
+    public void initialize() {
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        nicColumn.setCellValueFactory(new PropertyValueFactory<>("nic"));
+        licenseColumn.setCellValueFactory(new PropertyValueFactory<>("license"));
+        addressColumn.setCellValueFactory(new PropertyValueFactory<>("address"));
+        contactColumn.setCellValueFactory(new PropertyValueFactory<>("contact"));
+
+        loadDriver();
+    }
+
+    private void loadDriver() {
+        ObservableList<DriverTable> list = FXCollections.observableArrayList();
+        drivers.forEach(e->{
+            list.add(
+                    new DriverTable(e.getName(), e.getNic(), e.getLicense(), e.getAddress(), e.getContact())
+            );
+        });
+        driverTable.setItems(list);
+    }
+
     public void addDriver(MouseEvent mouseEvent) {
+        Driver driver = new Driver(
+                driverNameText.getText(),
+                nicText.getText(),
+                driverLicenseText.getText(),
+                addressText.getText(),
+                contactText.getText()
+        );
+        drivers.add(driver);
+        loadDriver();
     }
 }
--- src/controller/VehicleFormController.java
+++ src/controller/VehicleFormController.java
@@ -6,10 +6,10 @@ import com.jfoenix.controls.JFXTextField;
 import javafx.scene.input.MouseEvent;
 
 public class VehicleFormController {
+    public JFXComboBox vehicleComboBox;
     public JFXTextField vehicleNumberText;
     public JFXTextField maximumWeightText;
     public JFXTextField passengerText;
-    public JFXComboBox vehicleComboBox;
     public JFXButton addVehicleButton;
 
     public void addVehicle(MouseEvent mouseEvent) {
